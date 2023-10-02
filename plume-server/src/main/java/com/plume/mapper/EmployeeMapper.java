65e66563b8709f78f172cbd3ac31d6ca64fc7ff4
grayplume <birdgray@163,com>
2023-10-02 22:50:09 +0800

    禁用启用员工

--- plume-server/src/main/java/com/plume/mapper/EmployeeMapper.java
+++ plume-server/src/main/java/com/plume/mapper/EmployeeMapper.java
@@ -27,5 +27,16 @@ public interface EmployeeMapper {
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
 
+    /**
+     * 查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据主键动态修改属性
+     * @param employee
+     */
+    void update(Employee employee);
 }
