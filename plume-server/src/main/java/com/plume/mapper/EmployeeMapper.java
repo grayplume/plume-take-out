079330020766b4a23831cbeca6fc5eeeac04b462
grayplume <birdgray@163,com>
2023-10-02 19:55:52 +0800

    init

---
+++ plume-server/src/main/java/com/plume/mapper/EmployeeMapper.java
@@ -0,0 +1,18 @@
+package com.plume.mapper;
+
+import com.plume.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+}
