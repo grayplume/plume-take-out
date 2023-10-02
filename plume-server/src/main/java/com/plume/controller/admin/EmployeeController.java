b1536381f4bf7e75e9aa56ba04980399d8775f71
grayplume <birdgray@163,com>
2023-10-02 23:04:47 +0800

    更新员工信息

--- plume-server/src/main/java/com/plume/controller/admin/EmployeeController.java
+++ plume-server/src/main/java/com/plume/controller/admin/EmployeeController.java
@@ -116,4 +116,24 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    @ApiOperation("更新员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("更新员工信息：{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
