676adb23d7ce9fa3ceedf04dbcfd80fc4b6797f3
grayplume <birdgray@163,com>
2023-10-03 20:55:09 +0800

    新增套餐

---
+++ plume-server/src/main/java/com/plume/controller/admin/SetmealController.java
@@ -0,0 +1,38 @@
+package com.plume.controller.admin;
+
+import com.plume.dto.SetmealDTO;
+import com.plume.result.Result;
+import com.plume.service.SetmealService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Api(tags = "套餐相关接口")
+@Slf4j
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO) {
+        setmealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+}
