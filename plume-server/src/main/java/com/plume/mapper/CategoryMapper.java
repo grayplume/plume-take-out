e3ef8c18997e1756f55cfb154f6eb0fad70dc915
grayplume <birdgray@163,com>
2023-10-03 16:45:22 +0800

    公共字段自动填充

--- plume-server/src/main/java/com/plume/mapper/CategoryMapper.java
+++ plume-server/src/main/java/com/plume/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.plume.mapper;
 
 import com.github.pagehelper.Page;
+import com.plume.annotation.AutoFill;
 import com.plume.enumeration.OperationType;
 import com.plume.dto.CategoryPageQueryDTO;
 import com.plume.entity.Category;
@@ -16,6 +17,7 @@ public interface CategoryMapper {
      * 插入数据
      * @param category
      */
+    @AutoFill(value = OperationType.INSERT)
     @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
@@ -39,6 +41,7 @@ public interface CategoryMapper {
      * 根据id修改分类
      * @param category
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Category category);
 
     /**
--- plume-server/src/main/java/com/plume/service/impl/EmployeeServiceImpl.java
+++ plume-server/src/main/java/com/plume/service/impl/EmployeeServiceImpl.java
@@ -84,13 +84,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         // 设置默认密码
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        // 设置时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
-
-        // 设置创建人和修改人
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        // // 设置时间
+        // employee.setCreateTime(LocalDateTime.now());
+        // employee.setUpdateTime(LocalDateTime.now());
+        //
+        // // 设置创建人和修改人
+        // employee.setCreateUser(BaseContext.getCurrentId());
+        // employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
@@ -152,8 +152,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO,employee);
 
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        // employee.setUpdateTime(LocalDateTime.now());
+        // employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }
