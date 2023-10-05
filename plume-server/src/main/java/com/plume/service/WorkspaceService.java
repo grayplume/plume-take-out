c011050bfd98490ebf720556c55a9b6a6b05e609
grayplume <birdgray@163,com>
2023-10-05 16:24:26 +0800

    工作台

---
+++ plume-server/src/main/java/com/plume/service/WorkspaceService.java
@@ -0,0 +1,37 @@
+package com.plume.service;
+
+import com.plume.vo.BusinessDataVO;
+import com.plume.vo.DishOverViewVO;
+import com.plume.vo.OrderOverViewVO;
+import com.plume.vo.SetmealOverViewVO;
+import java.time.LocalDateTime;
+
+public interface WorkspaceService {
+
+    /**
+     * 根据时间段统计营业数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    OrderOverViewVO getOrderOverView();
+
+    /**
+     * 查询菜品总览
+     * @return
+     */
+    DishOverViewVO getDishOverView();
+
+    /**
+     * 查询套餐总览
+     * @return
+     */
+    SetmealOverViewVO getSetmealOverView();
+
+}
