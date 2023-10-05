9a079267174113773cb9c068a2c191c5ba771335
grayplume <birdgray@163,com>
2023-10-05 15:10:41 +0800

    取消订单\再来一单

--- plume-server/src/main/java/com/plume/service/OrderService.java
+++ plume-server/src/main/java/com/plume/service/OrderService.java
@@ -45,4 +45,15 @@ public interface OrderService {
      */
     OrderVO details(Long id);
 
+    /**
+     * 用户取消订单
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
+
+    /**
+     * 再来一单
+     * @param id
+     */
+    void repetition(Long id);
 }
